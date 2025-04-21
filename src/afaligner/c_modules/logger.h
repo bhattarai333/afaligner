44427d5ff3736b597bf6a40a2891bb4eb493ccab
Josh <joshbhattarai1@gmail.com>
2025-04-21 16:59:15 -0400

    implement logging for sparse matrix

--- src/afaligner/c_modules/logger.h
+++ src/afaligner/c_modules/logger.h
@@ -8,11 +8,25 @@
 #define LOG_MEM_ENABLED  1
 #define LOG_DEBUG_ENABLED 1
 
+#if defined(_WIN32) || defined(__WIN32__)
+    #ifdef BUILDING_FASTDTWBD
+        #define EXPORT __declspec(dllexport)
+    #else
+        #define EXPORT __declspec(dllimport)
+    #endif
+#else
+    #ifdef BUILDING_FASTDTWBD
+        #define EXPORT __attribute__((visibility("default")))
+    #else
+        #define EXPORT
+    #endif
+#endif
+
 // Logging functions
-void log_info(const char *format, ...);
-void log_debug(const char *format, ...);
-void log_memory_alloc(const void *ptr, size_t size, const char *func, int line);
-void log_memory_free(const void *ptr, const char *func, int line);
+EXPORT void log_info(const char *format, ...);
+EXPORT void log_debug(const char *format, ...);
+EXPORT void log_memory_alloc(const void *ptr, size_t size, const char *func, int line);
+EXPORT void log_memory_free(const void *ptr, const char *func, int line);
 
 // Helper macros for memory logging
 #define LOG_ALLOC(ptr, size) log_memory_alloc(ptr, size, __func__, __LINE__)
