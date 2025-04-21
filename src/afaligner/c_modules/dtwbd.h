bffd22d4430b4f1ff863d237704760ae66ea63a7
Josh <joshbhattarai1@gmail.com>
2025-04-21 12:06:23 -0400

    generate dtwbd.h

---
+++ src/afaligner/c_modules/dtwbd.h
@@ -0,0 +1,40 @@
+#ifndef DTWBD_H
+#define DTWBD_H
+
+#include <stdlib.h>
+#include <float.h>
+#include <math.h>
+#include <stddef.h>
+
+// Matrix element used for DTW/BD computation
+typedef struct {
+    double distance;
+    ssize_t prev_i;
+    ssize_t prev_j;
+} D_matrix_element;
+
+// Main DTWBD function
+ssize_t dtwbd(
+    double **x, size_t n,
+    double **y, size_t m,
+    size_t dim,
+    size_t *window,
+    double skip_penalty,
+    size_t *path_buffer,
+    double *path_distance
+);
+
+// Helper functions
+double euclid_distance(double *x, double *y, size_t l);
+
+double get_distance(
+    void *matrix, size_t n, size_t m,
+    size_t *window, size_t i, size_t j,
+    int use_sparse
+);
+
+D_matrix_element get_best_candidate(D_matrix_element *candidates, size_t n);
+
+void reverse_path(size_t *path, ssize_t path_len);
+
+#endif // DTWBD_H
