0ae956b4a8f30a1c3e70d4a77b39f0f11e828a3e
Josh <joshbhattarai1@gmail.com>
2025-04-21 15:02:59 -0400

    Create separate fastdtwdb.h

--- src/afaligner/c_modules/fastdtwbd.h
+++ src/afaligner/c_modules/fastdtwbd.h
@@ -1,18 +1,34 @@
 #ifndef FASTDTWBD_H
 #define FASTDTWBD_H
 
+#include <stdlib.h>
+#include <float.h>
+#include <math.h>
 #include <stddef.h>
+#include "dtwbd.h"  // Including dtwbd.h for shared structures and functions
 
-ssize_t FastDTWBD(
-    const double* s,
-    const double* t,
-    size_t n,
-    size_t m,
-    size_t l,
-    double skip_penalty,
-    int radius,
-    double* path_distance,
-    size_t* path_buffer
+#ifdef BUILDING_FASTDTWBD
+    #define EXPORT __attribute__((visibility("default")))  // Ensure the symbol is visible
+#else
+    #define EXPORT
+#endif
+
+// FastDTWBD function prototype (make sure it's marked with EXPORT)
+EXPORT ssize_t FastDTWBD(
+    double *s,  // first sequence of MFCC frames – n x l contiguous array
+    double *t,  // second sequence of MFCC frames – m x l contiguous array
+    size_t n,   // number of frames in first sequence
+    size_t m,   // number of frames in second sequence
+    size_t l,   // number of MFCCs per frame
+    double skip_penalty,    // penalty for skipping one frame
+    int radius,             // radius of path projection
+    double *path_distance,  // place to store warping path distance
+    size_t *path_buffer     // buffer to store resulting warping path – (n+m) x 2 contiguous array
 );
 
-#endif
+// Additional helper function prototypes if needed for FastDTWBD implementation
+EXPORT double *get_coarsed_sequence(double *s, size_t n, size_t l);
+EXPORT size_t *get_window(size_t n, size_t m, size_t *path_buffer, size_t path_len, int radius);
+EXPORT void update_window(size_t *window, size_t n, size_t m, ssize_t i, ssize_t j);
+
+#endif // FASTDTWBD_H
